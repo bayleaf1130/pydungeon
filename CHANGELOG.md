debf8538e86348298725628f9467c562aafe884c
Bailey Kremiller-Kocin <bailey.kocin@gmail.com>
2020-04-23 21:42:58 -0400

    Initial pydungeon files

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+#### 1.0.0 
+    [Author] Bailey Kremiller-Kocin [Date] 4/23/2020
+        - Added package structure
+        - Created package files
---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open('AUTHOR', 'r') as file:
+    authors = file.read().strip()
+
+with open('README.md', 'r') as file:
+    long_description = file.read().strip()
+
+with open('VERSION', 'r') as file:
+    version = file.read()
+
+setuptools.setup(
+    name='pydungeon', # The name
+    author=authors,
+    version=version,
+    description='Python D & D Library',
+    long_description=long_description,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7'
+    entry_points={
+        'console_scripts': ['dungeon=pydungeon:__main__']
+    }
+)
