41ee4d0338b2c75d59695e6b35f5ab117b80bc18
Yuriy <Sorlondo@Gmail.com>
2020-04-30 15:11:59 -0400

    skeleton of gui added

--- pydungeon/gui/application.py
+++ pydungeon/gui/application.py
@@ -1,5 +1,10 @@
+
 from kivy.app import App
-from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.graphics import Color, Rectangle
+from kivy.core.window import Window
 
 '''
 Kivy:
@@ -15,8 +20,44 @@ python -m pip install kivy==1.11.1
 python kivy_venv\share\kivy-examples\demo\showcase\main.py
 '''
 
-class GuiApplication(App):
-    def build(self):
-        return Button(text='Hello World')
 
-#GuiApplication().run()
+class GuiApplication(App):
+
+    def build(self):
+        self.window_settings()
+        return LoginScreen()
+
+    def window_settings(self):
+        Window.maximize()
+
+
+class LoginScreen(GridLayout):
+
+    def __init__(self, **kwargs):
+        super(LoginScreen, self).__init__(**kwargs)
+        self.cols = 2
+        self.add_widgets()
+    
+        with self.canvas.before:
+            Color(0, 0, 1, 1) # green; colors range from 0-1 instead of 0-255
+            self.rect = Rectangle(size=self.size,
+                                pos=self.pos)
+        
+        self.bind(pos=self.update_rect, size=self.update_rect)
+
+    
+    def add_widgets(self):
+        self.add_widget(Label(text='Character Name'))
+        self.username = TextInput(multiline=False)
+        self.add_widget(self.username)
+        self.add_widget(Label(text='Race'))
+        self.race = TextInput(multiline=False)
+        self.add_widget(self.race)
+        self.add_widget(Label(text='Class'))
+        self.characterClass = TextInput(multiline=False)
+        self.add_widget(self.characterClass)
+
+    
+    def update_rect(self, instance, value):
+        instance.rect.pos = instance.pos
+        instance.rect.size = instance.size
