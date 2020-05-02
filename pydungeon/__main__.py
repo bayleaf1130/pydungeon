e7bcaed07733ecde5a62d04ada3f05d09f3c2548
Yuriy <Sorlondo@Gmail.com>
2020-05-02 17:21:04 -0400

    Fixed Saving Throws

--- pydungeon/__main__.py
+++ pydungeon/__main__.py
@@ -6,6 +6,7 @@ dwarf_character = Character()
 dwarf_character.name = "Dhunmic"
 dwarf_character.race = "Dwarf"
 dwarf_character.addFeatures("Dwarf", "Can dig really really big holes.")
+#dwarf_character.addSavingThrow('strength', 15)
 dwarf_character.addItem("Sword", "Big sword")
 
 print("All Information\n")
@@ -17,4 +18,10 @@ print(dwarf_character.getBasicInformation())
 print("Proficiencies\n")
 print(dwarf_character.getProficiencies())
 
-GuiApplication().run()
+print("Features\n")
+print(dwarf_character.characterFeatures)
+
+print("Saving Throws\n")
+print(dwarf_character.savingThrows)
+
+#GuiApplication().run()
